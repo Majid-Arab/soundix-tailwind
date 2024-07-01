1aab0c8b1a9e44fb9d647ea8edfb70f39b1c1579
Majid-Arab <arabmajid12@gmail.com>
2024-07-01 07:01:07 +0500

    h1 styling

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -52,7 +52,7 @@ function Navbar() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="">
-              <h1>Headphones</h1>
+              <h1 className="text-2xl font-semibold">Headphones</h1>
               <p>Logos From Top Brands</p>
             </div>
           </div>
