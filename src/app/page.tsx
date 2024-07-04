41a181b48d7666766736a69e3b02a34702f0d658
Majid-Arab <arabmajid12@gmail.com>
2024-07-04 16:52:05 +0500

    created carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,13 @@ import {
   IconPointFilled,
 } from "@tabler/icons-react";
 import Rating from "@/components/rating";
+import Slide from "../../public/avatar.png";
+import Slide1 from "../../public/headphone.png";
+import Slide2 from "../../public/headphone1.png";
 
 function Home() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center">
       <div className="flex justify- items-center gap-5 h-[400px] w-[1000px] p-4 rounded-2xl shadow-2xl">
         <div className="px-5 ">
           <span className="">
@@ -74,6 +77,101 @@ function Home() {
             </button>
           </div>
         </div>
+      </div>
+
+      <div
+        id="controls-carousel"
+        className="relative w-full"
+        data-carousel="static"
+      >
+        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <Image
+              src={Slide}
+              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+          <div
+            className="hidden duration-700 ease-in-out"
+            data-carousel-item="active"
+          >
+            <Image
+              src={Slide}
+              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <Image
+              src={Slide1}
+              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <Image
+              src={Slide2}
+              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+            <Image
+              src={Slide}
+              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              alt="..."
+            />
+          </div>
+        </div>
+        <button
+          type="button"
+          className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-prev
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 1 1 5l4 4"
+              />
+            </svg>
+            <span className="sr-only">Previous</span>
+          </span>
+        </button>
+        <button
+          type="button"
+          className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+          data-carousel-next
+        >
+          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg
+              className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 6 10"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 9 4-4-4-4"
+              />
+            </svg>
+            <span className="sr-only">Next</span>
+          </span>
+        </button>
       </div>
     </div>
   );
